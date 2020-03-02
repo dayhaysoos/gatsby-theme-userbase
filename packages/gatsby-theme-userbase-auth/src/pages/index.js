e03918c88c8b3a8fb2491b0abb66fd2344e7fe23
dayhaysoos <ndejesus1227@gmail.com>
2020-03-02 18:17:31 -0500

    version bump

--- packages/gatsby-theme-userbase-auth/src/pages/index.js
+++ packages/gatsby-theme-userbase-auth/src/pages/index.js
@@ -15,7 +15,7 @@ const Index = () => {
           alignItems: 'center',
         }}
       >
-        <h1>Welcome to the gatsby-theme-userbase-auth demo!</h1>
+        <h4>Welcome to the gatsby-theme-userbase-auth demo!</h4>
         {user.username ? <h2>ty for signing in</h2> : <h2>Please sign in</h2>}
       </Flex>
     </Layout>
